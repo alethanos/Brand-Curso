b51038ad15e19c63a7f2b3a8313cba08b110200f
alethanos <78930128+alethanos@users.noreply.github.com>
2023-01-08 12:54:08 -0400

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,40 @@
+// <!-- <hr>
+//     <h2 class="mention_title">Menciones</h2> -->
+//     <!-- <div class="mentions">
+//       <div class="mention">
+//         <div class="content_mention">
+//           <img src="img/p1.png" alt="">
+//           <div class="text">
+//             <h3>Mario</h3>
+//             <p>Miles de productos hay en el mercado, pero nada como una caja con gatitos</p>
+//           </div>
+//         </div>
+//       </div>
+//       <div class="mention">
+//         <div class="content_mention">
+//           <img src="img/p2.png" alt="">
+//           <div class="text">
+//             <h3>Sonia</h3>
+//             <p>Miles de productos hay en el mercado, pero nada como una caja con gatitos</p>
+//           </div>
+//         </div>
+//       </div>
+//       <div class="mention">
+//         <div class="content_mention">
+//           <img src="img/p3.png" alt="">
+//           <div class="text">
+//             <h3>Maria</h3>
+//             <p>Miles de productos hay en el mercado, pero nada como una caja con gatitos</p>
+//           </div>
+//         </div>
+//       </div>
+//       <div class="mention">
+//         <div class="content_mention">
+//           <img src="img/p4.png"">
+//           <div class="text">
+//             <h3>Mario</h3>
+//             <p>Miles de productos hay en el mercado, pero nada como una caja con gatitos</p>
+//           </div>
+//         </div>
+//       </div>
+//     </div> -->
